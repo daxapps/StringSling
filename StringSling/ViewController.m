8eb9ae1eb2d8e5172529a0aea6a2e36f6d96235d
daxapps <daxapps777@gmail.com>
2017-01-30 06:32:06 -0600

    ObjectiveC Intro Tutorial Devslopes

--- StringSling/ViewController.m
+++ StringSling/ViewController.m
@@ -16,7 +16,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *firstName = @"John";
+    NSString *allocatedName = [[NSString alloc]init];
+    
+    NSLog(@"Name: %@", firstName);
+    
+    NSString *fullName = [NSString stringWithFormat:@"%@ Smith %@", firstName, @"Charles the III"];
+    
+    NSString *display = [fullName stringByAppendingString:@" - Died 1448"];
+    
+    NSLog(@"Fullname: %@", fullName);
+    
+    NSLog(@"%@", display);
+    
+    NSString *var1 = @"Junk";
+    NSString *var2 = @"in the trunk";
+    NSString *var3 = @"junk";
+    
+    if (![var1 isEqualToString:var2]) {
+        NSLog(@"We should see this!");
+    }
+    
+    if ([var1 caseInsensitiveCompare:var3] == NSOrderedSame) {
+        NSLog(@"Woohoo");
+    }
+    
 }
 
 
